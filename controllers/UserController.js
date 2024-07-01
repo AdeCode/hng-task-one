04b062c3a8a3e6a854b6d1fcdf4db500a0a2492b
adecode <agorohabeeb@gmail.com>
2024-07-01 13:18:40 +0200

    imtegrated weather API for temperature

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,17 +1,23 @@
 const axios = require('axios')
 
-const baseURL = 'https://ipgeolocation.abstractapi.com/v1'
+const locationRL = 'https://ipgeolocation.abstractapi.com/v1'
+const weatherAPIKey = "fc8df2ac1063bf50b0de268a5564e492";
 
 const getUserData = async(req, res) => {
     try{
-        const response = await axios.get(`${baseURL}/?api_key=${process.env.LOCATION_KEY}`)
-        // const resWithIp = await axios.get(`${baseURL}/?api_key=${process.env.LOCATION_KEY}?ip_address=${response.data.ip_address}`)
-        // console.log(resWithIp)
+        const response = await axios.get(`${locationRL}/?api_key=${process.env.LOCATION_KEY}`)
+       
+        const getWeather = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${response.data.city}&appid=${weatherAPIKey}&units=metric`
+          );
+
+        const temp = Math.round(getWeather.data.main.temp)
         res.status(200).json({ 
             client_ip: response.data.ip_address,
             location: response.data.city,
-            greeting: `Hello, ${req.query.visitor_name}!, the temperature is 11 degrees Celcius in ${response.data.city}` 
+            greeting: `Hello, ${req.query.visitor_name}!, the temperature is ${temp} degrees Celcius in ${response.data.city}` 
         })
+
     }catch(error){
         console.log(error)
         return res.status(400).json({ error: error })
